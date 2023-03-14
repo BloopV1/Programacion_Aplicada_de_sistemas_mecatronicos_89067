2bad89a392b582f19998dea90c25c8ce7262717d
BloopV1 <110635528+BloopV1@users.noreply.github.com>
2023-03-14 19:54:38 +0000

    starting

--- Parcial/30000087856_Gokú/Segundo:Punto.py
+++ Parcial/30000087856_Gokú/Segundo:Punto.py
@@ -1,11 +1,11 @@
-from math import sqrt 
+
 
 n = int(input("Escriba un número: "))
-def fibonacci(n):
+def Fibonacci(n):
     if n < 2:
         return n
     else:
         u = ((1+sqrt(n))/2)
         j = ((u**n-(1-u)**n)/sqrt(n))
         return round(j)
-print(fibonacci)
+print(Fibonacci)
