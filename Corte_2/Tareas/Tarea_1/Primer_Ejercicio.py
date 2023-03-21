c5399b9acbded3ac5eda810777a62cf8e5c2f11e
BloopV1 <110635528+BloopV1@users.noreply.github.com>
2023-03-21 03:09:30 +0000

    starting

--- Corte_2/Tareas/Tarea_1/Primer_Ejercicio.py
+++ Corte_2/Tareas/Tarea_1/Primer_Ejercicio.py
@@ -1,19 +1,18 @@
-# Realice un función llamada Aleatorio() donde se retorne un número entre 100 y 120, excepto los números 110, 115 y 119.
-# Al final se deben imprimir 10 números retornados de la función Aleatorio(), alternando el orden par, impar (Comenzando con número par).
+#Realice un función llamada Aleatorio() donde se retorne un número entre 100 y 120, excepto los números 110, 115 y 119. 
+#Al final se deben imprimir 10 números retornados de la función Aleatorio(), alternando el orden par, impar (Comenzando con número par).
 from random import randint
 
 def Aleatorio(n=10):
-    num = []
-    while len(num) < n:
-        Alea = randint(100, 120)
-
-    if (Alea != 110) and (Alea != 115) and (Alea != 119):
-        if (len(num) % 2) == 0 and (Alea % 2 == 0):
-            num.append(Alea)
-
-    elif (len(num) % 2) == 1 and (Alea % 2 == 1):
-        num.append(Alea)
+    num=[]
+    while len(num)<n:
+        a=randint(100, 120)
+        if (a!= 110) and (a!=115) and (a!=119):
+           if (len(num)%2)==0 and (a%2==0):
+             num.append(a)
+    
+        elif (len(num)%2)==1 and (a%2==1):
+            num.append(a)
     return num
 
-num = Aleatorio()
+num=Aleatorio()
 print(num)
