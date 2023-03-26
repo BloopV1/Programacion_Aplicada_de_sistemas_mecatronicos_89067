0e375f05c07b1e112e5af58ce6ee59e663876a73
BloopV1 <110635528+BloopV1@users.noreply.github.com>
2023-03-26 19:58:27 +0000

    starting

--- Corte_2/Tareas/Tarea_2/Ejercicio_2.py
+++ Corte_2/Tareas/Tarea_2/Ejercicio_2.py
@@ -1,9 +1,12 @@
 # Realice una función recursiva que calcule la siguiente función: 
 #  K(n,p) = 1*p +2*p+3*p + ... + n*p
 
-def calculo_de_K(n,p):
+def calculo_de_K(n,p): #En esta función, utilizamos la recursión para calcular la función K(n,p). 
     if n==n:
-        return p
-    else:
+        return p #Si n es igual a 1, entonces K(n,p) es igual a p, por lo que devolvemos p.
+    else:#De lo contrario, sumamos n*p al resultado de la función calcular_K(n-1, p) y lo devolvemos.
         return n*p+calculo_de_K(n-1,p)
 print (4*2 + calculo_de_K(3,2))
+
+
+
--- Corte_2/Tareas/Tarea_2/Ejercicio_3.py
+++ Corte_2/Tareas/Tarea_2/Ejercicio_3.py
@@ -1,9 +1,8 @@
-#Realice una función recursiva que calcule el factorial o el número de fibonacci 
-#(dependiendo del ejercicio que le salió en el parcial).
+#Realice una función recursiva que calcule el factorial o el número de fibonacci (dependiendo del ejercicio que le salió en el parcial).
 
-def fibonacci(n):
-    if n <= 1:
+def fibonacci(n): #Esta función toma un argumento, n, que representa el número de Fibonacci para calcular. 
+    if n <= 1: #Si n es menor o igual a 1, la función simplemente devuelve n.
         return n
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
-print (fibonacci)
+        return fibonacci(n-1) + fibonacci(n-2) # Si n es mayor que 1, la función se llama a sí misma dos veces con n-1 y n-2 como argumentos y luego suma los resultados para obtener el número de Fibonacci deseado.
+print(fibonacci(10))
