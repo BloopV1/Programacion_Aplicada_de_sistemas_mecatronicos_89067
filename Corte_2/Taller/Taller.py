8f85a36b4ac414c13f726f4951de76b413c1c6bb
BloopV1 <110635528+BloopV1@users.noreply.github.com>
2023-04-07 20:54:09 +0000

    starting

---
+++ Corte_2/Taller/Taller.py
@@ -0,0 +1,29 @@
+# Realice un programa para calcular el valor bruto de un producto alimenticio según la última reforma tributaria del IVA de la canasta familiar del año 2023.
+# leer txt y crear diccionario
+archivo = open("Alimentos.txt","r")
+contenido = archivo.read
+
+
+Al_dic = {}  
+with open("Alimentos.txt", "r") as file:
+    for line in file:
+        item, iva = line.strip().split(",")
+        Al_dic[item] = float(iva)
+
+while True:
+    producto= input("Ingrese el nombre del producto o escriba 'salir' para salir: ") #ingrese el nombre del producto y su valor neto
+    if producto == "salir":
+        break
+    valor_neto = float(input("Ingrese el valor neto del producto: "))
+# busca el producto en el diccionario y encuentra el producto buscado
+    if producto in Al_dic: 
+        valor_iva = valor_neto * Al_dic[producto]
+    else:
+        print("El producto ingresado no se encuentra en la lista.")
+        continue
+    valor_base = valor_neto + valor_iva 
+
+    #imprimir el valor base y el valor del IVA 
+    print(f"El valor base del producto es: {valor_base:.2f}")
+    print(f"El valor del IVA es: {valor_iva:.2f}")
+archivo.close()
---
+++ Corte_2/Tareas/Tarea_3.py
@@ -0,0 +1,31 @@
+#Realizar un ejercicio donde se calcule el IMC de las siguientes 4 personas, cuyos valores se ven en la tabla.
+#El sistema debe mostrar según el valor de IMC obtenido para cada uno de las personas indicando su correspondiente escala. 
+
+# NOMBRE ESTATURA(cm) PESO(kg)
+# Pedro     188         97
+# Maria     160         47
+# Julian    158         58
+#Jessica    170         73
+
+def calcular_imc(peso, altura):
+    return peso / (altura ** 2)
+
+personas = [ #Tupla de personas
+    {"nombre": "Pedro", "peso": 97, "altura": 1.88},
+    {"nombre": "María", "peso": 47, "altura": 1.60},
+    {"nombre": "Julian", "peso": 58, "altura": 1.58},
+    {"nombre": "Jessica", "peso": 73, "altura": 1.73}]
+
+for persona in personas: #calcular el IMC de cada persona
+    imc = calcular_imc(persona["peso"], persona["altura"])
+    # Porcentaje de IMC
+    if imc < 18.5:
+        print("Bajo peso")
+    elif imc < 25:
+        print("Peso normal")
+    elif imc < 30:
+        print("Sobrepeso")
+    else:
+        print("Obesidad")
+
+print(f"{persona['nombre']}: IMC = {imc:.2f}")
